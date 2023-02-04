52c90ada8d85c1218989e10f73bc378303c41e79
Nirob Hasan <nirob427417@gmail.com>
2023-02-04 15:42:52 +0600

    post data mongodb

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -7,11 +7,26 @@ const AddUser = () => {
     const handleAddUser = event => {
         event.preventDefault();
         console.log(user);
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    alert('User added successfully');
+                    event.target.reset();
+                }
+            })
     }
 
     const handleInputBlur = event => {
-        const value = event.target.value;
         const field = event.target.name;
+        const value = event.target.value;
         const newUser = { ...user }
         newUser[field] = value;
         setUser(newUser);
