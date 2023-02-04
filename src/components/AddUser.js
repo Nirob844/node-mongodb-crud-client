c7b7e2a59ecb4c74a9e2b0040aeee1da51fe1812
Nirob Hasan <nirob427417@gmail.com>
2023-02-04 15:13:44 +0600

    Get User Data From Client

---
+++ src/components/AddUser.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const AddUser = () => {
+
+    const [user, setUser] = useState({});
+
+    const handleAddUser = event => {
+        event.preventDefault();
+        console.log(user);
+    }
+
+    const handleInputBlur = event => {
+        const value = event.target.value;
+        const field = event.target.name;
+        const newUser = { ...user }
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
+
+    return (
+        <div>
+            <h3>Add new user </h3>
+            <form onSubmit={handleAddUser}>
+                <input onBlur={handleInputBlur} type="text" name='name' placeholder='name' required />
+                <br />
+                <input onBlur={handleInputBlur} type="text" name='address' placeholder='address' required />
+                <br />
+                <input onBlur={handleInputBlur} type="email" name="email" id="" placeholder='email' required />
+                <br />
+                <button type="submit">Add User</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
